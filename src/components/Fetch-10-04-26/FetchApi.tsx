11fe427ca826c2d27c8fb1b0ae922802b5bc3604
venkatasagar-axiora <venkatasagar.t@axioraglobalsolutions.com>
2026-04-14 09:45:10 +0530

    Api Calling Products

--- src/components/Fetch-10-04-26/FetchApi.tsx
+++ src/components/Fetch-10-04-26/FetchApi.tsx
@@ -1,39 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import './FetchApi.css'
+
 
 function FetchApi() {
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const controller = new AbortController();
-
-    fetch('https://fakestoreapi.com/products', {
-      signal: controller.signal
-    })
+    fetch("https://fakestoreapi.com/products")
       .then(res => res.json())
-      .then(data => {
-        setProducts(data);
-      })
-      .catch(err => {
-        if (err.name === 'AbortError') {
-          console.log('Request cancelled');
-        } else {
-          console.log(err);
-        }
-      });
-
-    return () => {
-      controller.abort(); // 🔥 cleanup
-    };
+      .then(data => setData(data));
   }, []);
 
   return (
-    <div>
-        <h1>Products</h1>
-      {/* {products.map(p => (
-        <p key={p.id}>{p.title}</p>
-      ))} */}
+    <div className="card-products">
+      <div className="container-fluid">
+        <div className="row">
+          {data.map((item: any) => (
+            <div className="col-12 col-md-3 " key={item.id}>
+              <div className="card h-100">
+                <div className="card-header">
+                  <img src={item.image} alt="" className="card-img-top" height='200px' />
+                </div>
+                <div className="card-body">
+                  <h6>{item.title}</h6>
+                  <p>{item.description.substring(0, 80)}</p>
+                  <p className="badge bg-warning text-dark">rating: {item.rating['rate']}</p>
+                  <br></br>
+                  <label htmlFor="" className="badge bg-success">${item.price}</label>
+                </div>
+                <div className="card-footer text-center">
+                  <button className="btn btn-primary">Buy</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
 export default FetchApi;
